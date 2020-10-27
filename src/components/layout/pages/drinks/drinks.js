52da6863af94d75046ff1d9adae799864ac4f63b
Luca <Lucaargentieri-98@hotmail.it>
2020-10-27 16:12:14 +0100

    Step back, restart

--- src/components/layout/pages/drinks/drinks.js
+++ src/components/layout/pages/drinks/drinks.js
@@ -1,42 +1,7 @@
-import React, { Component } from 'react'
-import DrinksContainer from './drinksContainer/drinksContainer'
+import React from 'react'
 
-import axios from 'axios'
-
-
-class Drinks extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            loading: true,
-            drinksItems: []
-        }
-    }
-    // res.data
-
-    componentDidMount() {
-        axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic')
-            .then(res => 
-                this.setState({ drinksItems: res.data })
-            )
-            .then(res => console.log(this.state.drinksItems))
-
-            
-
-    }
-    render(){
-        if (this.state.loading) {
-            return <p>loading...</p>
-        }
-
-        if (!this.state.drinks) {
-            return <p>Didn't get a drink</p>
-        } else {
-        return(
-            <DrinksContainer drinks={this.state.drinksItems} />
-        )
-    }
-    }
+function Drinks() {
+    return <h2>Drinks!</h2>
 }
 
 
