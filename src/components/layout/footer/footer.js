d1e63f639f116359fb065d6e13f6b8835612338b
Luca Argentieri <Lucaargentieri-98@hotmail.it>
2021-01-26 18:01:48 +0100

    Update footer.js

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css'
 export default function Footer() {
     return (
         <footer>
-            <p>Made with <span>❤</span> and hops</p>
+            
         </footer>
     )
 }
